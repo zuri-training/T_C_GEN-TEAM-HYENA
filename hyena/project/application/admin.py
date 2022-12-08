50af63c44f864a4d1c403f695d0e45e26fb566dd
OFOH OGHENERUONA WEALTH <114484362+OfohRuona96@users.noreply.github.com>
2022-12-07 16:30:10 -0800

    Delete hyena/project directory

--- hyena/project/application/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import  Company, Company_policies ,Company_policy_contacts
-
-# Register your models here.
-
-admin.site.register(Company_policies )
-admin.site.register(Company)
-admin.site.register(Company_policy_contacts)
